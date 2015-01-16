9a31c34f83ecba63a96f7d06bf79a7e5d7503012
Ricardo Canales <canalesb93@gmail.com>
2015-01-16 14:34:05 -0600

    Stats Ajax improvements

--- src/app.js
+++ src/app.js
@@ -9,6 +9,47 @@ var ajax = require('ajax');
 var UI = require('ui');
 var Settings = require('settings');
 
+// ================================= Front-End =================================
+
+//Preloading Main Menu
+var menu = new UI.Menu({
+  sections: [{
+    items: [{
+      title: 'Loading...',
+      icon: 'images/lolstats.png'
+    }]
+  },{
+    title: 'Statistics',
+    items: [{
+      title: 'Ranked 5v5',
+      subtitle: 'Wins, Losses, Kills, Assits, Minion Kills'
+    },{
+      title: 'Unranked 5v5',
+      subtitle: 'Wins, Kills, Assits, Minions Killed, Turrets Killed'
+    }]
+  }]
+});
+
+//Preloading Ranked Menu
+var rankedSummary = new UI.Menu({
+  sections: [{
+    title: 'Ranked 5v5',
+    items: [{
+      title: 'Loading...' 
+    }]
+  }]
+});
+
+//Preloading UnRanked Menu
+var unrankedSummary = new UI.Menu({
+  sections: [{
+    title: 'Unranked 5v5',
+    items: [{
+      title: 'Loading...' 
+    }]
+  }]
+});
+
 // ================================ Alert Cards ================================
 // Settings card
 var settings = new UI.Card({
@@ -25,7 +66,8 @@ var warning = new UI.Card({
 // ================================= Settings ==================================
 var api_key = 'f07420f5-3ae0-4c25-b5cd-dcbdf7c71605';
 var options = Settings.option();
-var summoner;
+var summoner, stats;
+var rankedPresent = false;
 
 // Configurable with just the close callback
 function setSettings(){
@@ -62,29 +104,10 @@ function setSettings(){
   );
 }
 
+
+// =================================== Main ====================================
+
 setSettings();
-
-
-// ================================= Front-End =================================
-
-//Preloading Main Menu
-var menu = new UI.Menu({
-  sections: [{
-    items: [{
-      title: 'Loading...',
-      icon: 'images/lolstats.png'
-    }]
-  },{
-    title: 'Statistics',
-    items: [{
-      title: 'Ranked 5v5',
-      subtitle: 'Wins, Losses, Kills, Assits, Minion Kills'
-    },{
-      title: 'Unranked 5v5',
-      subtitle: 'Wins, Kills, Assits, Minions Killed, Turrets Killed'
-    }]
-  }]
-});
 
 menu.show();
 if(!options.username){
@@ -93,35 +116,13 @@ if(!options.username){
   summonerRequest();
 }
 
-//Preloading Ranked Menu
-var rankedSummary = new UI.Menu({
-  sections: [{
-    title: 'Ranked 5v5',
-    items: [{
-      title: 'Loading...' 
-    }]
-  }]
-});
-
-//Preloading UnRanked Menu
-var unrankedSummary = new UI.Menu({
-  sections: [{
-    title: 'Unranked 5v5',
-    items: [{
-      title: 'Loading...' 
-    }]
-  }]
-});
-
 menu.on('select', function(e) {
   console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
   console.log('The item is titled "' + e.item.title + '"');
 
   if(e.itemIndex === 0 && e.sectionIndex === 1){
-    rankedSummary.show();    
     rankedRequest();
   } else if(e.itemIndex === 1 && e.sectionIndex === 1){
-    unrankedSummary.show();    
     unrankedRequest();
   }
 });
@@ -148,14 +149,73 @@ function summonerRequest(){
 }
 
 function rankedRequest(){
+  if(!stats){
+    statsRequest();
+  }
+  if(rankedPresent){
+    rankedSummary.show();    
+  } else {
+    displayNoUnranked(); 
+  }
+}
+
+function unrankedRequest(){
+  if(!stats){
+    statsRequest();
+  }
+  unrankedSummary.show();    
+}
+
+function superTrim(name){
+  var newName = name.replace(/\s+/g, '');
+  newName = newName.toLowerCase();
+  return newName;
+}
+
+function displayNoRanked(){
+  warning.scrollable(false);
+  warning.subtitle('Ranked Data not found.');
+  warning.body('Play some ranked games to get this information.');
+  rankedSummary.hide();
+  warning.show();
+}
+
+function displayNoUnranked(){
+  warning.scrollable(false);
+  warning.subtitle('Unranked Data not found.');
+  warning.body('Play some unranked games to get this information.');
+  unrankedSummary.hide();
+  warning.show();
+}
+
+function statsRequest(){
   ajax({ url: 'https://'+options.region+'.api.pvp.net/api/lol/'+options.region+'/v1.3/stats/by-summoner/'+summoner.id+'/summary?season=SEASON4&api_key='+api_key, type: 'json' },
     function(data) {
-      console.log('Ajax succesful');
-      for (var i = 0; data.playerStatSummaries[i]; i++) { 
-        if(data.playerStatSummaries[i].playerStatSummaryType == 'RankedSolo5x5'){
+      stats = data;
+      console.log('Ajax Stats request succesful!');
+      for (var i = 0; data.playerStatSummaries[i]; i++) {
+        console.log('type: ' + data.playerStatSummaries[i].playerStatSummaryType);
+        if(data.playerStatSummaries[i].playerStatSummaryType == 'Unranked'){
+          var sectionOne = {
+            title: 'Unranked 5v5',
+            items: [{
+              title: 'Wins: ' + data.playerStatSummaries[i].wins
+            },{
+              title: 'Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalChampionKills,
+              subtitle: 'Assists: ' + data.playerStatSummaries[i].aggregatedStats.totalAssists
+            },{
+              title: 'Minion Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalMinionKills,
+              subtitle: 'Neutral Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalNeutralMinionsKilled
+            },{
+              title: 'Turret Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalTurretsKilled
+            }]
+          };
+          unrankedSummary.section(0, sectionOne);
+        } else if(data.playerStatSummaries[i].playerStatSummaryType == 'RankedSolo5x5'){
           if(data.playerStatSummaries[i].wins === 0 && data.playerStatSummaries[i].losses === 0){
-            displayNoRanked();
+            console.log('No ranked games found');
           } else {
+            rankedPresent = true;
             var sectionOne = {
               title: 'Ranked Solo 5v5',
               items: [{
@@ -179,61 +239,8 @@ function rankedRequest(){
     }, 
     function(error){
       console.log('The ajax request failed: ' + error);
+      unrankedSummary.item(0,0,{ title: 'Loading Failed'});
       rankedSummary.item(0,0,{ title: 'Loading Failed'});
     }
   );
-}
-
-function unrankedRequest(){
-  ajax({ url: 'https://'+options.region+'.api.pvp.net/api/lol/'+options.region+'/v1.3/stats/by-summoner/'+summoner.id+'/summary?season=SEASON4&api_key='+api_key, type: 'json' },
-    function(data) {
-      console.log('Ajax succesful');
-      for (var i = 0; data.playerStatSummaries[i]; i++) {
-        console.log('type: ' + data.playerStatSummaries[i].playerStatSummaryType);
-        if(data.playerStatSummaries[i].playerStatSummaryType == 'Unranked'){
-          var sectionOne = {
-            title: 'Unranked 5v5',
-            items: [{
-              title: 'Wins: ' + data.playerStatSummaries[i].wins
-            },{
-              title: 'Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalChampionKills,
-              subtitle: 'Assists: ' + data.playerStatSummaries[i].aggregatedStats.totalAssists
-            },{
-              title: 'Minion Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalMinionKills,
-              subtitle: 'Neutral Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalNeutralMinionsKilled
-            },{
-              title: 'Turret Kills: ' + data.playerStatSummaries[i].aggregatedStats.totalTurretsKilled
-            }]
-          };
-          unrankedSummary.section(0, sectionOne);
-        }  
-      }
-    }, 
-    function(error){
-      console.log('The ajax request failed: ' + error);
-      unrankedSummary.item(0,0,{ title: 'Loading Failed'});
-    }
-  );
-}
-
-function superTrim(name){
-  var newName = name.replace(/\s+/g, '');
-  newName = newName.toLowerCase();
-  return newName;
-}
-
-function displayNoRanked(){
-  warning.scrollable(false);
-  warning.subtitle('Ranked Data not found.');
-  warning.body('Play some ranked games to get this information.');
-  rankedSummary.hide();
-  warning.show();
-}
-
-function displayNoUnranked(){
-  warning.scrollable(false);
-  warning.subtitle('Unranked Data not found.');
-  warning.body('Play some unranked games to get this information.');
-  unrankedSummary.hide();
-  warning.show();
 }
